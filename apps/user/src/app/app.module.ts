681aabaa016569f4c2daf975d67efb971e294b58
Alexander <aleksandry11@gmail.com>
2023-04-14 12:42:10 +0300

    init

--- apps/user/src/app/app.module.ts
+++ apps/user/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
+import { KafkaService } from 'libs/kafka/src/lib/kafka.service';
+
 @Module({
-  imports: [],
+  imports: [KafkaService],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, KafkaService],
 })
 export class AppModule {}
